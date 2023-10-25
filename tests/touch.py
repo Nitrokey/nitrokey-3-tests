635fd6e6f8a210b3ce3944552eb0f79e7a55cdef
Robin Krahl <robin@nitrokey.com>
2023-10-25 20:30:48 +0200

    Mark touch tests as virtual
    The touch tests only make sense with virtual devices as we cannot change
    the touch behavior of a hardware device from the test suite.

--- tests/touch.py
+++ tests/touch.py
@@ -1,12 +1,15 @@
 # Copyright (C) 2022 Nitrokey GmbH
 # SPDX-License-Identifier: CC0-1.0
 
+import pytest
 from .basic import TestFido2, TestFido2Resident
 
 
+@pytest.mark.virtual
 def test_fido2(touch_device):
     TestFido2().run(touch_device)
 
 
+@pytest.mark.virtual
 def test_fido2_resident(touch_device):
     TestFido2Resident().run(touch_device)
